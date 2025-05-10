087461ce088222c880e509a86a08e7a5734a1a85
z.khaddan <zakaria.khaddan@it-adaptive.com>
2025-05-10 16:31:53 +0100

    argo file

--- test1-service/src/main/java/com/adaptive/test1service/Test1ServiceApplication.java
+++ test1-service/src/main/java/com/adaptive/test1service/Test1ServiceApplication.java
@@ -2,9 +2,17 @@ package com.adaptive.test1service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Test1ServiceApplication {
+
+    @GetMapping("/mess")
+    public String getMessage() {
+        return "Welcome to Adaptive-It..!!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Test1ServiceApplication.class, args);
--- test2-service/src/main/java/com/adaptive/test2service/Test2ServiceApplication.java
+++ test2-service/src/main/java/com/adaptive/test2service/Test2ServiceApplication.java
@@ -2,9 +2,17 @@ package com.adaptive.test2service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Test2ServiceApplication {
+
+    @GetMapping("/api/message2")
+    public String getMessage() {
+        return "Hello team Adaptive-It..!!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Test2ServiceApplication.class, args);
